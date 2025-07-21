063edcc07f2f6b495d5f3f2bc33eb3dbe1ad21a0
kumagaias <14195829+kumagaias@users.noreply.github.com>
2025-07-22 08:28:56 +0900

    fix: css path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
+    basePath: '/typing-game',
+    assetPrefix: '/typing-game',
     images: {
         unoptimized: true
     }
